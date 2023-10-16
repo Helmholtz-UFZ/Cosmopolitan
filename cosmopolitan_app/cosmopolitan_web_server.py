07590bdcc5d0c9ba8285bbabeb90e936bb5686d6
john <john-eric.anders@ufz.de>
2023-10-16 14:28:40 +0200

    Add error page and move error handling to error handler.

--- cosmopolitan_app/cosmopolitan_web_server.py
+++ cosmopolitan_app/cosmopolitan_web_server.py
@@ -4,10 +4,12 @@ import os
 import traceback
 from logging.config import dictConfig
 
+import sqlalchemy
 from flask import Flask, render_template, request
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException, NotFound
 
 from cosmopolitan_app.config import DEBUG
+from cosmopolitan_app.cosmopolitan_job import InvalidJobID
 from cosmopolitan_app.cosmopolitan_job_form import json_load_4_jinja
 from cosmopolitan_app.dash_component import dynamic_plots
 from cosmopolitan_app.dash_component.dash_component import init_dash
@@ -37,6 +39,50 @@ app.config["MAX_CONTENT_LENGTH"] = 5 * 1024 * 1024 * 1024  # 5 Gb limit
 app.jinja_env.globals.update(json_loads=json_load_4_jinja)
 
 
+def error_response(e):
+    """Handle standard errors."""
+    if isinstance(e, JobNotFound):
+        app.logger.info("Handle JobNotFound exception")
+        return (
+            render_template("html/errors/job_not_found_error.html", job_id=e.job_id),
+            500,
+        )
+
+    if isinstance(e, InvalidJobID):
+        app.logger.info("Handle InvalidJobID exception")
+        return (
+            render_template("html/errors/job_not_found_error.html", job_id=e.job_id),
+            500,
+        )
+
+    if isinstance(e, sqlalchemy.exc.OperationalError):
+        app.logger.info("Handle sqlalchemy.exc.OperationalError exception")
+        log_error()
+        return render_template("html/errors/db_no_connection_error.html"), 500
+
+    if isinstance(e, NotFound):
+        app.logger.info("Handle NotFound exception")
+        return render_template("html/errors/file_not_found.html"), 404
+
+
+def log_error():
+    """
+    Log error with traceback.
+
+    In production this will trigger an email, see logger.py.
+    """
+    route = request.url_rule
+    route_function = request.endpoint
+
+    error = traceback.format_exc()
+    content = (
+        f"Unexpected error in { route } using { route_function }:\n"
+        f"{error}\n"
+        f"PID={os.getpid()}\n"
+    )
+    app.logger.error(content)
+
+
 @app.errorhandler(Exception)
 def handle_exception(e):
     """
@@ -54,14 +100,17 @@ def handle_exception(e):
     Returns:
         HTTPException or tuple: Depending on the type of exception, this function
         returns an appropriate HTTPException or a tuple containing a rendered error
-        template and a 500 status code.
+        template and a status code.
 
     Note:
         This function should be registered as an error handler in the Flask app
         using `@app.errorhandler(Exception)`.
     """
-    if isinstance(e, JobNotFound):
-        return render_template("html/errors/job_not_found_error.html", job_id="a"), 500
+    app.logger.info("Handle exception")
+    handled_response = error_response(e)
+
+    if handled_response:
+        return handled_response
 
     if app.debug:
         raise e
@@ -69,17 +118,7 @@ def handle_exception(e):
     if isinstance(e, HTTPException):
         return e
 
-    route = request.url_rule
-    route_function = request.endpoint
-
-    app.logger.info("Handle exception")
-    error = traceback.format_exc()
-    content = (
-        f"Unexpected error in { route } using { route_function }:\n"
-        f"{error}\n"
-        f"PID={os.getpid()}\n"
-    )
-    app.logger.error(content)
+    log_error()
     return render_template("html/errors/internal_error.html"), 500
 
 
