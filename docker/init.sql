ba5ecd5a05aa4436099e90b3c2f55cd92eb91bed
john <john-eric.anders@ufz.de>
2024-01-09 14:41:36 +0100

    prod env update

--- docker/init.sql
+++ docker/init.sql
@@ -34,8 +34,11 @@ CREATE TABLE logs (
 GRANT INSERT, UPDATE, DELETE ON TABLE jobs TO somweb_stage_rw;
 GRANT INSERT, UPDATE, DELETE ON TABLE logs TO somweb_stage_rw;
 
+-- GRANT INSERT, UPDATE, DELETE ON TABLE jobs TO somweb_prod_rw;
+-- GRANT INSERT, UPDATE, DELETE ON TABLE logs TO somweb_prod_rw;
 -- SELECT job_id, start_date FROM jobs;
 -- SELECT job_id, status FROM jobs;
 -- SELECT job_id, cluster_job_id FROM jobs;
+-- psql -U somweb_prod_adm -p 5432 -h postgres-dev.intranet.ufz.de -d somweb_prod
 -- psql -U somweb_stage_adm -p 5432 -h postgres-dev.intranet.ufz.de -d somweb_stage
 -- psql -U somweb_stage_rw -p 5432 -h localhost -d somweb_stage
