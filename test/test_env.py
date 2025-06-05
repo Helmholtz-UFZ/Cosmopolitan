4fbf3b7dd70aae60b7e506bbd78d56f19cdd8029
john <john-eric.anders@ufz.de>
2025-06-05 09:33:25 +0200

    finished testing dash duo

--- test/test_env.py
+++ test/test_env.py
@@ -48,7 +48,6 @@ def test_env_prod(logger):
         f.write("POSTGRES_PASSWORD='password'\n")
         f.write("MINIO_ACCESS_KEY='password'\n")
         f.write("MINIO_SECRET_KEY='password'\n")
-        f.write("MAINTAINER_EMAIL='test@test.de'\n")
 
     # Remove the env_vars from the environment
     for env_var in env_vars:
